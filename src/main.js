22821333c279223d45093088cccc64d5eb98e434
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2018-01-15 11:25:11 -0800

    Add App render vue instance

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueAxios from 'vue-axios';
 import axios from 'axios';
+import App from './App.vue';
 
 Vue.use(VueRouter);
 Vue.use(VueAxios, axios);
 
 const router = new VueRouter({ mode: 'history' });
-new Vue(Vue.util.extend({ router })).$mount('#app');
+new Vue(Vue.util.extend({ router, App })).$mount('#app');
